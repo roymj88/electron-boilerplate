a05ef7fc079245c7dbcf71fbfb1ede746f93dee5
Roy M J <roymj88@gmail.com>
2016-08-02 15:25:44 +0530

    Updating changes

--- app/hello_world/hello_world.js
+++ app/hello_world/hello_world.js
@@ -5,3 +5,24 @@ export var greet = function () {
 export var bye = function () {
     return 'See ya!';
 };
+$(document).on("click", ".submit", function(){
+	var firstname 	= $(".firstname").val();
+	var lastname 	= $(".lastname").val();
+
+	if(!firstname){
+		alert("First Name is required!");
+	}else if(!lastname){
+		alert("Last Name is required!");
+	}else{
+
+		var html  = "<tr>";
+			html += "<td>"+firstname+"</td>";
+			html += "<td>"+lastname+"</td>";
+			html += "</tr>";
+
+		$("table.list").find("tbody").append(html);
+		$(".firstname").val("");
+		$(".lastname").val("");
+	}
+
+});
